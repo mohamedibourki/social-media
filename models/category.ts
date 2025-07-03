1cf96e21978e067c74ae17d1e082d0c4208fa340
Mohamed Ibourki <63525865+mohamedibourki@users.noreply.github.com>
2025-07-03 17:45:50 +0100

    Update category.ts

--- models/category.ts
+++ models/category.ts
@@ -1,12 +1,5 @@
 import mongoose, { Document, Schema } from "mongoose";
-
-export interface ICategory extends Document {
-  nom: string;
-  description?: string;
-  pole_id: mongoose.Types.ObjectId;
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { ICategory } from "../interfaces/category";
 
 const categorySchema = new Schema<ICategory>(
   {
